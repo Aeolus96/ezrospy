a4c11285dd26a9259477f1b26f35140ab6b1deb3
PranavMLTU <pmalik@ltu.edu>
2025-05-26 15:19:09 -0400

    Update f1-1_static_pedestrian_detection.py
    Added test description

--- scripts/f1-1_static_pedestrian_detection.py
+++ scripts/f1-1_static_pedestrian_detection.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
+# Test FI.1 Static Pedestrian Detection
+# 1. Test Goal
+    # This test is intended to evaluate detection of a mannequin using traditional Machine Vision
+    # algorithms. A mannequin wears ORANGE construction vest. A GUI interface with extracted
+    # orange blob MUST be present during a run. There are NO PENALTIES for crossing or moving
+    # over a line.
+# 2. Test Setup
+    # The following items shall be placed on the road:
+    # o Barrel 1 to indicate starting point at which vehicle is stationary
+# 3. Test Script
+    # 1. Begin test run
+    # 2. Judge pushes 'start' button
+    # 3. The extracted orange blob is present on the screen.
+    # 4. End test run
+# 4. Evaluation
+    # Fail Criteria – no GUI interface is present during the run, incorrect identification of the shape/object
+    # Penalties – no penalties for crossing or moving over the lines, in case if vehicle is moving during the test
 
 import actor_ros  # ACTor specific utility functions
 import rospy  # ROS Python API
