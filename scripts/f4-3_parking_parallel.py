0ee2ae7ff0f36f1bcc451d085e51b963eac4ba31
PranavMLTU <pmalik@ltu.edu>
2025-05-27 11:37:00 -0400

    Update f4-3_parking_parallel.py
    Updated test description formatting

--- scripts/f4-3_parking_parallel.py
+++ scripts/f4-3_parking_parallel.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Test FIV.3. Parking. Parallel
 # 1. Test Goal
   # This test is intended to evaluate if a vehicle is able to parallel park into the representative parking space. The
@@ -7,7 +8,7 @@
   # all 3 attempts.
 # 2. Test Setup
   # The following items shall be placed on the road:
-  # o Barrel 1 to indicate starting point at which vehicle is stationary
+  # - Barrel 1 to indicate starting point at which vehicle is stationary
 # 3. Test Script
   # 1. Begin test run
   # 2. Judge pushes 'start' button
@@ -17,6 +18,7 @@
   # 6. End test run
 # 4. Evaluation
   # Fail Criteria – vehicle crosses solid white line
+
 
 import actor_ros  # ACTor specific utility functions
 import rospy  # ROS Python API
