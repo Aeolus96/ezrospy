4dc9ffe14e7e3e864901d1b55005a993ba300892
PranavMLTU <pmalik@ltu.edu>
2025-05-27 11:25:53 -0400

    Update q0_estop_manual.py
    Formatting update

--- scripts/q0_estop_manual.py
+++ scripts/q0_estop_manual.py
@@ -12,20 +12,20 @@ from modules.ezros_robot import Schoolbus
 
 # Test Q.0 E-Stop Manual
 # 1. Test Goal
-# This test is intended to evaluate safety features of Manual E-Stop.
+    # This test is intended to evaluate safety features of Manual E-Stop.
 # 2. Test Setup
-# The following items shall be placed on the road:
-# o Barrel 1 on the side of the road to indicate a starting point at which vehicle is stationary
-# o Barrel 2 on the side of the road to indicate the position where E-Stop button is pressed
+    # The following items shall be placed on the road:
+    # o Barrel 1 on the side of the road to indicate a starting point at which vehicle is stationary
+    # o Barrel 2 on the side of the road to indicate the position where E-Stop button is pressed
 # 3. Test Script
-# 1. Begin test run
-# 2. Judge pushes 'start' button
-# 3. Vehicle takes off from full stop at Barrel 1
-# 4. Vehicle maintains the target speed
-# 5. Judge manually pushes E-Stop and reaches full stop within 3ft (+- 2 inches) form the Barrel 2
-# 6. End test run
+    # 1. Begin test run
+    # 2. Judge pushes 'start' button
+    # 3. Vehicle takes off from full stop at Barrel 1
+    # 4. Vehicle maintains the target speed
+    # 5. Judge manually pushes E-Stop and reaches full stop within 3ft (+- 2 inches) form the Barrel 2
+    # 6. End test run
 # 4. Evaluation
-# Pass Criteria - vehicle reaches full stop within 3 ft (+- 2 inches) from Barrel 2.
+    # Pass Criteria - vehicle reaches full stop within 3 ft (+- 2 inches) from Barrel 2.
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
